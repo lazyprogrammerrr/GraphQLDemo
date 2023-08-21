5e4f954dcc24127d6fe1629e8159495ee5697530
Lazyprogramerrr <aakashsharma344@gmail.com>
2023-08-21 22:20:48 +0530

    Updated Query with DTO

--- GraphQLDemo.API/Schema/Querys/Query.cs
+++ GraphQLDemo.API/Schema/Querys/Query.cs
@@ -1,34 +1,66 @@
 ﻿using Bogus;
+using GraphQLDemo.API.DTOs;
+using GraphQLDemo.API.Services.Courses;
 using static GraphQLDemo.API.Models.Subject;
 
 namespace GraphQLDemo.API.Schema.Querys
 {
     public class Query
     {
+        private readonly CourseRepository _courseRepository;
+
+        public Query(CourseRepository courseRepository)
+        {
+            _courseRepository = courseRepository;
+        }
+
         public Faker<InstructorType> _instructorFaker;
         public Faker<StudentType> _studentFaker;
         public Faker<CourseType> _courseFaker;
-        public Query()
+        //public Query()
+        //{
+        //    _instructorFaker = new Faker<InstructorType>().RuleFor(c => c.Id,f=> Guid.NewGuid()).RuleFor(c=>c.FirstName,f=>f.Name.FirstName()).RuleFor(c=>c.LastName,f=>f.Name.LastName()).RuleFor(c=>c.Salary,f=>f.Random.Double(0,10000));
+
+        //    _studentFaker = new Faker<StudentType>().RuleFor(c => c.Id, f => Guid.NewGuid()).RuleFor(c => c.FirstName, f => f.Name.FirstName()).RuleFor(c => c.LastName, f => f.Name.LastName()).RuleFor(c => c.GPA, f => f.Random.Int(1,10));
+
+        //    _courseFaker = new Faker<CourseType>().RuleFor(c => c.Id, f => Guid.NewGuid()).RuleFor(c => c.Name, f => f.Name.JobTitle()).RuleFor(c => c.Subject, f => f.PickRandom<SubjectsEnum>()).RuleFor(c => c.Instructor, f => _instructorFaker.Generate()).RuleFor(c=>c.Students,f=>_studentFaker.Generate(3));
+
+        //}
+
+        public async Task<IEnumerable<CourseType>> GetCourses()
         {
-            _instructorFaker = new Faker<InstructorType>().RuleFor(c => c.Id,f=> Guid.NewGuid()).RuleFor(c=>c.FirstName,f=>f.Name.FirstName()).RuleFor(c=>c.LastName,f=>f.Name.LastName()).RuleFor(c=>c.Salary,f=>f.Random.Double(0,10000));
-
-            _studentFaker = new Faker<StudentType>().RuleFor(c => c.Id, f => Guid.NewGuid()).RuleFor(c => c.FirstName, f => f.Name.FirstName()).RuleFor(c => c.LastName, f => f.Name.LastName()).RuleFor(c => c.GPA, f => f.Random.Int(1,10));
-
-            _courseFaker = new Faker<CourseType>().RuleFor(c => c.Id, f => Guid.NewGuid()).RuleFor(c => c.Name, f => f.Name.JobTitle()).RuleFor(c => c.Subject, f => f.PickRandom<SubjectsEnum>()).RuleFor(c => c.Instructor, f => _instructorFaker.Generate()).RuleFor(c=>c.Students,f=>_studentFaker.Generate(3));
-
-        }
-
-        public IEnumerable<CourseType> GetCourses()
-        {
-            return _courseFaker.Generate(5);
+            IEnumerable<CourseDTO> courseDTOs = await _courseRepository.GetAll();
+            return courseDTOs.Select(c => new CourseType()
+            {
+                Id = c.Id,
+                Name = c.Name,
+                Subject = c.Subject,
+                Instructor = new InstructorType()
+                {
+                    Id = c.Instructor.Id,
+                    FirstName = c.Instructor.FirstName,
+                    LastName = c.Instructor.LastName,
+                    Salary = c.Instructor.Salary
+                }
+            });
         }
 
         public async Task<CourseType> GetCourseById(Guid id)
         {
-            await Task.Delay(5);
-            CourseType course = _courseFaker.Generate();
-            course.Id = id;
-            return course;
+            CourseDTO courseDTOs = await _courseRepository.GetById(id);
+            return new CourseType()
+            {
+                Id = courseDTOs.Id,
+                Name = courseDTOs.Name,
+                Subject = courseDTOs.Subject,
+                Instructor = new InstructorType()
+                {
+                    Id = courseDTOs.Instructor.Id,
+                    FirstName = courseDTOs.Instructor.FirstName,
+                    LastName = courseDTOs.Instructor.LastName,
+                    Salary = courseDTOs.Instructor.Salary
+                }
+            };
         }
 
         [GraphQLDeprecated("This query is Depreciated")]
